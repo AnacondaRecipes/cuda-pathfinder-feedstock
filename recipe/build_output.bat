16c2e5071ffaba23e28e7cc04e8dab91e86c9cbe
Onur Bingol <obingol@anaconda.com>
2025-10-31 11:06:42 -0700

    no cython dependency, so no temp file cleaning is needed

--- recipe/build_output.bat
+++ recipe/build_output.bat
@@ -2,7 +2,3 @@
 set DIR_NAME=%PKG_NAME:-=_%
 cd %DIR_NAME%
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
-
-:: Clean up Cython temporary files from site-packages directory
-del /s /q %SP_DIR%\*.c
-del /s /q %SP_DIR%\*.cpp
--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -4,7 +4,3 @@
 DIR_NAME="$(echo $PKG_NAME | tr '-' '_')"
 cd $DIR_NAME
 $PYTHON -m pip install . --no-deps --no-build-isolation -vv
-
-# Clean up Cython temporary files from site-packages directory
-find $SP_DIR -name "*.c" -type f -delete
-find $SP_DIR -name "*.cpp" -type f -delete
